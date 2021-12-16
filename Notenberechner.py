e595a6379c1508c02068827ea0b1f2ac925ea826
Nik864 <96227757+Nik864@users.noreply.github.com>
2021-12-16 09:10:12 +0100

    Update Notenberechner.py

--- Notenberechner.py
+++ Notenberechner.py
@@ -1,10 +1,21 @@
 Noten=[] #Eine Liste wird erstellt
+##################################################################################
+
+
+#Das Programm fragt den Benutzer unendlich oft nach einer Note, die das Programm danach in die Liste speicher kann
 while True:
-    Note=float(input("Gib deine Note ein: ")) #Eine Note von 1-6 kann eingetragen werden.
-    Noten.append(Note) #Die Note wird in die Liste "Noten" hinzugefügt.
-    print("Noten: " + str(Noten)) #Alle Noten werden ausgeschrieben.
-    summe=0#Summe wird auf 0 gesetzt.
-    for Note in Noten:#Für alle Note in Noten.
-        summe= summe + Note#Die Summe wird mit der Note addiert.
-        Durchschnitt=summe/len(Noten)#Die Summe wird durch die Anzahl Noten dividiert.
-    print("Durchschnitt: " +str(Durchschnitt))#Der Durchschnitt wird gedruckt
+    Note=float(input("Gib deine Note ein: ")) 
+    Noten.append(Note) 
+    print("Noten: " + str(Noten)) 
+    summe=0
+    
+    
+#############################################################################
+#In der for-Schleife wird der Durchschnitt berechnet, indem es alle Noten zuammenrechnet und danach den Durchschnitt berechnet.    
+    for Note in Noten:
+        summe= summe + Note
+        Durchschnitt=summe/len(Noten)
+######################################################################################
+
+#Der Durchschnitt wird gedruckt
+ print("Durchschnitt: " +str(Durchschnitt))
