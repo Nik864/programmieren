1d17ec209645af2ad329fde8401d819c6c632c47
Nik864 <96227757+Nik864@users.noreply.github.com>
2021-12-16 08:59:18 +0100

    Update Notenberechner.py

--- Notenberechner.py
+++ Notenberechner.py
@@ -1,10 +1,10 @@
-Noten=[]
+Noten=[] #Eine Liste wird erstellt
 while True:
-    Note=float(input("Gib deine Note ein: "))
-    Noten.append(Note)
-    print("Noten: " + str(Noten))
-    summe=0
-    for Note in Noten:
-        summe= summe + Note
-        Durchschnitt=summe/len(Noten)
-    print("Durchschnitt: " +str(Durchschnitt))
+    Note=float(input("Gib deine Note ein: ")) #Eine Note von 1-6 kann eingetragen werden.
+    Noten.append(Note) #Die Note wird in die Liste "Noten" hinzugefügt.
+    print("Noten: " + str(Noten)) #Alle Noten werden ausgeschrieben.
+    summe=0#Summe wird auf 0 gesetzt.
+    for Note in Noten:#Für alle Note in Noten.
+        summe= summe + Note#Die Summe wird mit der Note addiert.
+        Durchschnitt=summe/len(Noten)#Die Summe wird durch die Anzahl Noten dividiert.
+    print("Durchschnitt: " +str(Durchschnitt))#Der Durchschnitt wird gedruckt
